cf4010c52e663f8accc200e26362333476e834e4
MookizsHub <mookies2004@gmail.com>
2024-01-30 09:30:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<!-- 
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+setInterval(() => {
+  count.value = count.value + 1
+  console.log(count)
+  console.log(count.value)
+}, 1000)
+</script>
+<template>
+  <div>Count: {{ count }}</div>
+</template>
+<style scoped></style> -->
+
+<!-- ref -->
+<script setup>
+import { ref } from 'vue'
+//reactive on object variable
+const student = ref({ id: 1, name: 'Somsak' })
+//correct
+console.log(student.value.id)
+console.log(student.value.name)
+//incorrect
+console.log(student.id)
+console.log(student.name)
+//reactive on array variable
+const ids = ref([111, 123, 333, 555])
+//correct
+console.log(ids.value.length)
+console.log(ids.value[0])
+console.log(ids.value[ids.value.length - 1])
+//incorrect
+console.log(ids.length)
+</script>
+<template>
+  <div></div>
+</template>
+<style scoped></style>
